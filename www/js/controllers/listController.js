cdeca4bae864f56d06b5213739357fd96ad133c9
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-07-01 20:41:58 -0600

    list page loads posts

--- www/js/controllers/listController.js
+++ www/js/controllers/listController.js
@@ -1,4 +1,19 @@
 angular.module('dropzio')
-.controller('ListController', function($state, $scope){
+.controller('ListController', function($state, $scope, ListService){
+
+  $scope.postList;
+
+
+
+  ListService.getPosts()
+  .then(function(result){
+
+
+    console.log(result);
+    $scope.postList = result;
+
+
+
+  })
 
 })
--- www/js/services/listService.js
+++ www/js/services/listService.js
@@ -1,8 +1,24 @@
 angular.module('dropzio')
 
+.service('ListService', function($q, $http){
+
+  return {
+
+    getPosts: function (){
+      var deferred = $q.defer();
+      $http.get('https://dropzio-server.herokuapp.com/postlist')
+      .then(function(success){
+        deferred.resolve(success.data.post)
+      })
+      .catch(function(error){
+        deferred.reject(error)
+      })
+      return deferred.promise;
+    }
+
+  }
 
 
 
-.service('ListService', [function(){
 
-}]);
+});
