bf0df215d2a717173954613bbed5fcdab10c77d6
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-07-06 17:54:53 -0600

    compass

--- www/js/controllers/compassController.js
+++ www/js/controllers/compassController.js
@@ -1,4 +1,77 @@
 angular.module('dropzio')
 .controller('CompassController', function($state, $scope){
 
+
+  $scope.currentLocation;
+  $scope.destination = {};
+  $scope.bearing;
+  $scope.currentLocation = {
+    "compass": {
+    }
+  };
+
+  var watchOptions = {
+    timeout: 1000,
+    enableHighAccuracy: true
+  };
+
+  $cordovaGeolocation
+  .getCurrentPosition(watchOptions)
+  .then(function (position) {
+    $scope.currentLocation.compass.lat = position.coords.latitude;
+    $scope.currentLocation.compass.long = position.coords.longitude;
+  })
+  .then(function(done){
+
+    $scope.findNearbyPosts = function(){
+
+      CompassService.getNearbyPosts($scope.currentLocation)
+      .then(function(res1){
+        console.log(res1.data.posts[0].lat);
+        console.log(res1.data.posts[0].long);
+
+        $scope.destination.lat = res1.data.posts[0].lat;
+        $scope.destination.long = res1.data.posts[0].long;
+      })
+
+    }
+  })
+
+
+  $interval(function(){
+
+    $cordovaDeviceOrientation.getCurrentHeading()
+    .then(function(orientResult){
+      if ($scope.angleDeg + orientResult.magneticHeading >= 360) {
+        $scope.bearing = $scope.angleDeg + orientResult.magneticHeading - 360
+      } else {
+        $scope.bearing = $scope.angleDeg + orientResult.magneticHeading
+      }
+    })
+
+    var watchOptions = {
+      timeout: 1000,
+      enableHighAccuracy: false
+    };
+
+    $cordovaGeolocation
+    .getCurrentPosition(watchOptions)
+    .then(function (position) {
+      $scope.currentLocation.compass.lat = position.coords.latitude;
+      $scope.currentLocation.compass.long = position.coords.longitude;
+    })
+
+
+    $scope.angleDeg = Math.atan2($scope.destination.lat - $scope.currentLocation.compass.lat, $scope.destination.long - $scope.currentLocation.compass.long) * 180 / Math.PI
+    // console.log(orientation);
+    angular.element(document.querySelector('#needle')).css('-webkit-transform', 'rotate(' + $scope.bearing + 'deg)' )
+
+
+  }, 500);
+
+
+
+
+
+
 })
